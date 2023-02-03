e844ccdd32ab5a77af909805792d19253faecbdd
Rahmi Rahmiev <rahmi.rahmiev@yahoo.com>
2023-02-03 15:38:23 +0000

    Adding changes to logic.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,1 @@
-API_KEY = "pk.eyJ1Ijoicm9zYXpodSIsImEiOiJja2ZvbTFvbzEyM2c1MnVwbTFjdmVycXk5In0.71jVP2vD8pBWO2bsKtI48Q";
+API_KEY = "pk.eyJ1IjoiZGFuemFub3JpYSIsImEiOiJja2xzbmRtdW0wMG5vMndwOWZ5eHB0aGg1In0.jUlWUL4j8TYa5ESWbfJqnw";
